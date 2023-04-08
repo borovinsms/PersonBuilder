e232b95d93068c7fad5763e5bebfdf27cd1901f5
borovinsms <120593749+borovinsms@users.noreply.github.com>
2023-04-08 23:46:58 +0300

    Update Person.java

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -44,7 +44,11 @@ public class Person {
     }
 
     public OptionalInt getAge() {
-        return OptionalInt.of(age);
+        if (age != 0) {
+            return OptionalInt.of(age);
+        } else {
+            return OptionalInt.empty();
+        }
     }
 
     public String getAddress() {
@@ -67,10 +71,10 @@ public class Person {
     @Override
     public String toString() {
         return "Person{" +
-                "name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", age=" + age +
-                ", address='" + address + '\'' +
+                "name='" + getName() + '\'' +
+                ", surname='" + getSurname() + '\'' +
+                ", age=" + getAge() +
+                ", address='" + getAddress() + '\'' +
                 '}';
     }
 
