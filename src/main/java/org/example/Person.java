bdb790afde1957f1afac5f91cd59bd135cac4dd2
borovinsms <120593749+borovinsms@users.noreply.github.com>
2023-04-08 22:15:34 +0300

    Add files via upload

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,81 @@
+package org.example;
+
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public Person(PersonBuilder personBuilder) {
+        this.name = personBuilder.name;
+        this.surname = personBuilder.surname;
+        this.age = personBuilder.age;
+        this.address = personBuilder.address;
+    }
+
+    public boolean hasAge() {
+        return age != 0;
+    }
+
+    public boolean hasAddress() {
+        return !address.isEmpty();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return OptionalInt.of(age);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+            age++;
+    }
+
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder().setSurname(this.surname).setAge(5).setAddress(this.address);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getSurname(), getAge(), getAddress());
+    }
+}
